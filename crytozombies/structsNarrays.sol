d8523b4d8a1d456d979d9de47527c2b2829107ee
Srutip04 <subuhishruti75@gmail.com>
2022-09-08 21:47:46 +0530

    structNArrays

---
+++ crytozombies/structsNarrays.sol
@@ -0,0 +1,18 @@
+
+contract ZombieFactory {
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function createZombie (string memory _name, uint _dna) public {
+        // start here
+        zombies.push(Zombie(_name,_dna));
+    }
+
+}
