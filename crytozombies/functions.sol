1dc13df6102383da4c42a89be93d35bc22e00e82
Srutip04 <subuhishruti75@gmail.com>
2022-09-08 21:46:54 +0530

    functions

---
+++ crytozombies/functions.sol
@@ -0,0 +1,18 @@
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // start here
+    function createZombie(string memory _name,uint _dna) public{
+        
+    }
+
+}
