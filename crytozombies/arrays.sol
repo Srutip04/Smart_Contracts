e1b5cd3a38864715f0878d24f21a545477e94f10
Srutip04 <subuhishruti75@gmail.com>
2022-09-08 21:46:40 +0530

    arrays

---
+++ crytozombies/arrays.sol
@@ -0,0 +1,15 @@
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // start here
+    
+    Zombie[] public zombies;
+
+}
