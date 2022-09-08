4022426a352bc96ceff8d8fbe71f44c704c1920a
Srutip04 <subuhishruti75@gmail.com>
2022-09-08 22:11:30 +0530

    return values

---
+++ crytozombies/returnvalues.sol
@@ -0,0 +1,22 @@
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    // start here
+    function _generateRandomDna(string memory _str) private view returns(uint){
+        
+    }
+
+}
