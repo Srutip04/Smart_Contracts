edb53f3f8e3a2a56783848b227cc0f1ba5fae8a9
Srutip04 <subuhishruti75@gmail.com>
2022-09-08 22:11:10 +0530

    private

---
+++ crytozombies/private.sol
@@ -0,0 +1,17 @@
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+}
