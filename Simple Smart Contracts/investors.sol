0f7f4011bcba8ffaf741f412f92c4e06b4b9dcc3
Srutip04 <subuhishruti75@gmail.com>
2022-11-02 17:24:04 +0530

    done!

--- Simple Smart Contracts/investors.sol
+++ Simple Smart Contracts/investors.sol
@@ -3,12 +3,27 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract InvestorsWallets{
+    uint fortune;
     address payable[] investorWallets;
     mapping(address => uint) investors;
+
+    constructor() payable{
+        fortune = msg.value;
+    }
 
     function payInvestors(address payable wallet ,uint amt) public{
         investorWallets.push(wallet);
         investors[wallet] = amt;
+    }
+
+    function payout() private {
+        for(uint i=0; i<investorWallets.length;i++){
+            investorWallets[i].transfer(investors[investorWallets[i]]);
+        }
+    }
+
+    function makePayment() public{
+        payout();
     }
 
     function checkInvestors() public view returns (uint) {
